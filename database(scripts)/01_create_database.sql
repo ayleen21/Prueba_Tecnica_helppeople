a9daf3b480b536878ec7c4afb24c07c8029bd7fb
ayleen21 <ayleenmurillo77@gmail.com>
2026-02-28 10:33:42 -0500

    chore(db): renombrar base de datos en script de inicialización

--- database(scripts)/01_create_database.sql
+++ database(scripts)/01_create_database.sql
@@ -1,10 +1,10 @@
 --Archivo: 01_create_database.sql
 
-IF DB_ID('CatalogoProductosDB') IS NULL
+IF DB_ID('CatalogoProductosBD') IS NULL
 BEGIN
-    CREATE DATABASE CatalogoProductosDB;
+    CREATE DATABASE CatalogoProductosBD;
 END
 GO
 
-USE CatalogoProductosDB;
+USE CatalogoProductosBD;
 GO
