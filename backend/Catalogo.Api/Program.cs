5d2bb1291e8109edd17a1edc3b4a6dab0ccae350
ayleen21 <ayleenmurillo77@gmail.com>
2026-02-28 07:06:04 -0500

    feat(api): configurar DbContext, conexión a SQL Server y Swagger

--- backend/Catalogo.Api/Program.cs
+++ backend/Catalogo.Api/Program.cs
@@ -1,18 +1,33 @@
+using Catalogo.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+//Servicios
+builder.Services.AddControllers();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configuracion dbcontext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Swagger siempre activo 
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
 
 var summaries = new[]
 {
